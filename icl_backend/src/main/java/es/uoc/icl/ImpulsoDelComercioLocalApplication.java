633e54f6d0e9722281d552cf13247406ea578642
Ricardo Vidal Ortiz <ricardovidalortiz@gmail.com>
2024-04-11 21:56:12 +0200

    Primer commit

---
+++ icl_backend/src/main/java/es/uoc/icl/ImpulsoDelComercioLocalApplication.java
@@ -0,0 +1,13 @@
+package es.uoc.icl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpulsoDelComercioLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpulsoDelComercioLocalApplication.class, args);
+	}
+
+}
