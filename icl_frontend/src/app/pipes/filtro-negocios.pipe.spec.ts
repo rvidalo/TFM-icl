3bf04b5687c015a8c57b03f7e29668025cb76386
Ricardo Vidal Ortiz <ricardovidalortiz@gmail.com>
2024-05-08 21:12:40 +0200

    Se elimina el pipe filtro de negocios

--- icl_frontend/src/app/pipes/filtro-negocios.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FiltroNegociosPipe } from './filtro-negocios.pipe';
-
-describe('FiltroNegociosPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FiltroNegociosPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
