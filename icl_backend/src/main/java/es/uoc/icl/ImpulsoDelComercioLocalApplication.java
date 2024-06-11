f8a864ec9b094c5b31be21ed3fe53feebd7f07a0
Ricardo Vidal Ortiz <ricardovidalortiz@gmail.com>
2024-06-11 20:32:05 +0200

    Se añade .env

--- icl_backend/src/main/java/es/uoc/icl/ImpulsoDelComercioLocalApplication.java
+++ icl_backend/src/main/java/es/uoc/icl/ImpulsoDelComercioLocalApplication.java
@@ -3,10 +3,19 @@ package es.uoc.icl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ImpulsoDelComercioLocalApplication {
 
 	public static void main(String[] args) {
+		// Cargar el archivo .env
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
+        // Iniciar la aplicación Spring Boot
 		SpringApplication.run(ImpulsoDelComercioLocalApplication.class, args);
 	}
 
